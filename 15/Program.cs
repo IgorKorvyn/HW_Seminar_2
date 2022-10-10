87888ac8165755b5c78d4a8d5af5bf2d31084e32
korvyn <korvyn@yandex.ru>
2022-10-10 21:15:57 +0300

    Created task15

---
+++ 15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите цифру дня недели ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n == 6 || n == 7)
+    Console.WriteLine("Выходной день");
+if(n == 5 || n == 4 || n == 3 || n == 2 || n == 1)
+    Console.WriteLine("Будний день");
+if(n < 1 || n > 7)
+    Console.WriteLine("Не день недели");
