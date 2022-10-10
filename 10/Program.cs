7c82c797549228c8386460f2a99235daaeed4ef8
korvyn <korvyn@yandex.ru>
2022-10-10 20:40:58 +0300

    Created task10

---
+++ 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n < 100 || n > 999)
+{
+Console.WriteLine("Введенное число не трехзначное, введите трехзначное число"); 
+}
+else
+{
+Console.Write(((n%100) - (n%10)) / 10);
+}
