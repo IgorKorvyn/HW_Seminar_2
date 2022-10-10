5d2ab279ecb3a4990984c1190c4ba8d6177a7e9b
korvyn <korvyn@yandex.ru>
2022-10-10 20:57:50 +0300

    Created task13

---
+++ 13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+string n2 = (n).ToString(); 
+if(n > 99)
+    Console.WriteLine(n2[2]);
+if(n < 100)
+    Console.WriteLine("Третьей цифры нет");
